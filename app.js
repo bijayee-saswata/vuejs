0ea0b1bf41f5f7e3ba7a3cd7da9771593959da43
bijayee-saswata <saswat.sipun@gmail.com>
2018-12-18 12:07:32 +0530

    data binding and directives

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name : 'hello'
+        name : 'hello',
+        website: 'www.google.com',
+        num: 25
+    },
+    methods: {
+        greet : function(msg){
+            return "From : "+ msg+ " says "+ this.name ;
+        },
+        add : function(){
+            return this.num++;
+        } 
     }
 });
